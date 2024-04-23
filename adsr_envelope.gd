a621df119aee5557b0e9af442e9561be2fce2d25
pkflunr <71987718+pkflunr@users.noreply.github.com>
2024-04-23 16:22:22 -0700

    added a signal to adsr envelope ui

--- adsr_envelope.gd
+++ adsr_envelope.gd
@@ -5,3 +5,7 @@ extends PanelContainer
 @onready var sustain_slider = %SustainSlider
 @onready var release_slider = %ReleaseSlider
 
+signal settings_changed(attack,decay,sustain,release)
+
+func _on_any_setting_changed(_value):
+	settings_changed.emit(attack_slider.value, decay_slider.value,sustain_slider.value,release_slider.value)
